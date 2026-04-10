c4c6dc85bf3abdcb95982a1d8130170ce8ba60a1
Z PC <zaineirshad.07@gmail.com>
2026-04-10 16:18:40 +0100

    Version 0.3.0

--- TodoList.java
+++ TodoList.java
@@ -2,14 +2,40 @@ import java.util.Scanner;
 import java.io.IOException;
 import java.util.ArrayList;
 
-
 public class TodoList {
     public static void main(String[] args) {
         ArrayList<String> tasks = new ArrayList<>();
+        ArrayList<String> removedTasks = new ArrayList<>();
+
         Scanner kbm = new Scanner(System.in);
 
         // Pre-populate the task list with 20 tasks
         // Purely for testing purposes
+        arrayAppend(tasks, "Call the dentist");
+        arrayAppend(tasks, "Walk the dog");
+        arrayAppend(tasks, "Read 10 pages of a book");
+        arrayAppend(tasks, "Buy groceries");
+        arrayAppend(tasks, "Clean the kitchen");
+        arrayAppend(tasks, "Reply to emails");
+        arrayAppend(tasks, "Go for a 20-minute run");
+        arrayAppend(tasks, "Schedule a meeting");
+        arrayAppend(tasks, "Do the laundry");
+        arrayAppend(tasks, "Water the plants");
+        arrayAppend(tasks, "Pay utility bills");
+        arrayAppend(tasks, "Organize desk");
+        arrayAppend(tasks, "Prepare dinner");
+        arrayAppend(tasks, "Check calendar for tomorrow");
+        arrayAppend(tasks, "Back up important files");
+        arrayAppend(tasks, "Review monthly budget");
+        arrayAppend(tasks, "Stretch for 10 minutes");
+        arrayAppend(tasks, "Update task list");
+        arrayAppend(tasks, "Take out the trash");
+        arrayAppend(tasks, "Refill water bottle");
+
+        arrayAppend(removedTasks, "Be cool");
+        arrayAppend(removedTasks, "Be Cooler");
+        arrayAppend(removedTasks, "Be Warm");
+        arrayAppend(removedTasks, "Be Warmer");
 
         String command = "";
 
@@ -28,7 +54,10 @@ public class TodoList {
             System.out.println("v = View all tasks");
             wait(100);
 
-            System.out.println("r = Remove a task");
+            System.out.println("d = Delete a task");
+            wait(100);
+
+            System.out.println("r = Resore a deleted task");
             wait(100);
 
             System.out.println("e = Exit");
@@ -38,12 +67,14 @@ public class TodoList {
             command = kbm.nextLine();
 
             try {
-                if (command.equals("a")) {
+                if (command.equalsIgnoreCase("a")) {
                     addTask(tasks, kbm);
                 } else if (command.equalsIgnoreCase("v")) {
                     viewTasks(tasks, kbm, false);
+                } else if (command.equalsIgnoreCase("d")) {
+                    removeTask(tasks, removedTasks, kbm);
                 } else if (command.equalsIgnoreCase("r")) {
-                    removeTask(tasks, kbm);
+                    restoreTask(tasks, removedTasks, kbm);
                 } else if (command.equalsIgnoreCase("e")) {
                     clearWithLinuxCommand();
                     System.out.println("Exiting the program. Goodbye!");
@@ -83,6 +114,7 @@ public class TodoList {
 
     }
 
+    // Clears terminal during excecution (only works on linux)
     public static void clearWithLinuxCommand() {
         try {
             new ProcessBuilder("clear")
@@ -100,10 +132,17 @@ public class TodoList {
         System.out.print("Enter the task: ");
         String newTask = kbm.nextLine();
 
-        arrayAppend(tasks, newTask);
-        wait(500);
-        System.out.println("Task has been added sucessfully");
-        wait(500);
+        if (!(checkDupes(tasks, newTask))) {
+            wait(500);
+            System.out.println("The task is already in the list");
+        } else {
+            arrayAppend(tasks, newTask);
+            wait(500);
+            System.out.println("Task has been added sucessfully");
+            wait(500);
+
+        }
+
         System.out.println("\nPress enter to return to the main menu");
         kbm.nextLine();
 
@@ -111,24 +150,45 @@ public class TodoList {
 
     }
 
+    public static boolean checkDupes(ArrayList<String> tasks, String input) {
+        for (int i = 0; i < tasks.size(); i++) {
+            if (tasks.get(i).equals(input)) {
+                return false;
+            }
+        }
+        return true;
+    }
+
     public static void arrayAppend(ArrayList<String> stringArray, String toAdd) {
         stringArray.add(toAdd);
     }
 
-    public static void arrayRemove(ArrayList<String> stringArray, int index, Scanner kbm) {
+    public static void arrayCopy(ArrayList<String> from, ArrayList<String> to, int index) {
+        String tranferValue = from.get(index - 1);
+        to.add(tranferValue);
+    }
+
+    public static void arrayRemove(ArrayList<String> stringArray, int index, Scanner kbm,boolean move) {
         stringArray.remove(index - 1);
 
+        if(move){
         wait(500);
+        System.out.println("The task has been moved");
+        }
+        else{
+                  wait(500);
         System.out.println("The task has been removed");
+  
+        }
 
-        wait(200);
+        wait(100);
         System.out.println("Press enter to return to the main menu");
         kbm.nextLine();
         clearWithLinuxCommand();
 
     }
 
-    public static void viewTasks(ArrayList<String> tasks, Scanner kbm, boolean IsRemoval) {
+    public static void viewTasks(ArrayList<String> tasks, Scanner kbm, boolean JustShow) {
         clearWithLinuxCommand();
 
         System.out.println("Loading all tasks");
@@ -138,31 +198,33 @@ public class TodoList {
             System.out.println("\nPress enter to return to the main menu");
             kbm.nextLine();
         } else {
-            System.out.println("\n=== All Tasks === \n");
-            wait(500);
-
-            for (int i = 0; i < tasks.size(); i++) {
-                System.out.println((i + 1) + ". " + tasks.get(i));
-                wait(200);
-            }
-            wait(500);
-
-            if (IsRemoval) {
+            if (!JustShow) {
+                System.out.println("\n=== All Tasks === \n");
+                wait(500);
 
             } else {
-                System.out.println("\n === List Printed ===\n");
-                wait(200);
-
-                System.out.println("Press enter to return to the main menu");
-                kbm.nextLine();
 
             }
+            for (int i = 0; i < tasks.size(); i++) {
+                System.out.println((i + 1) + ". " + tasks.get(i));
+                wait(100);
+            }
+            wait(500);
+
+            if (!JustShow) {
+                System.out.println("\n === List Printed ===\n");
+                wait(100);
+                System.out.println("Press enter to return to the main menu");
+                kbm.nextLine();
+            } else {
+
+            }
+
         }
 
     }
 
-    public static void removeTask(ArrayList<String> tasks, Scanner kbm) {
-        int newOrder = 1;
+    public static void removeTask(ArrayList<String> tasks, ArrayList<String> removed, Scanner kbm) {
         viewTasks(tasks, kbm, true);
         // if empty make it go back
         if (tasks.size() == 0) {
@@ -172,25 +234,53 @@ public class TodoList {
             String newTask = kbm.nextLine();
             int taskNum = Integer.parseInt(newTask);
             clearWithLinuxCommand();
-            System.out.println("\n This is what your list will look like:\n");
-            for (int i = 0; i < tasks.size(); i++) {
-                if (i == taskNum - 1) {
-                    continue;
-                } else {
-                    System.out.println(newOrder + ". " + tasks.get(i));
-                    wait(200);
-                    newOrder++;
 
-                }
+            boolean isRemoved = showNewList(tasks, taskNum, kbm);
+
+            if (isRemoved) {
+                arrayCopy(tasks, removed, taskNum);
+                arrayRemove(tasks, taskNum, kbm,false);
             }
 
-            System.out.println("\nIs this the change you want to make? (y/n)");
-            String saveornah = kbm.nextLine();
+        }
 
-            if (saveornah.equals("y")) {
-                arrayRemove(tasks, taskNum, kbm);
+    }
+
+    public static boolean showNewList(ArrayList<String> tasks, int task, Scanner kbm) {
+        int newOrder = 1;
+
+        System.out.println("\n This is what your list will look like:\n");
+        for (int i = 0; i < tasks.size(); i++) {
+            if (i == task - 1) {
+                continue;
+            } else {
+                System.out.println(newOrder + ". " + tasks.get(i));
+                wait(100);
+                newOrder++;
+
             }
         }
+
+        System.out.println("\nIs this the change you want to make? (y/n)");
+        String saveornah = kbm.nextLine();
+
+        if (saveornah.equalsIgnoreCase("y")) {
+            return true;
+        } else {
+            return false;
+        }
+    }
+
+    public static void restoreTask(ArrayList<String> tasks, ArrayList<String> removedItems, Scanner kbm) {
+        viewTasks(removedItems, kbm, true);
+        System.out.println("\nThese are your removed tasks.");
+
+        System.out.print("\nEnter the number for the task you want to restore: ");
+        String newTask = kbm.nextLine();
+        int taskNum = Integer.parseInt(newTask);
+
+        arrayCopy(removedItems, tasks, taskNum);
+        arrayRemove(removedItems, taskNum, kbm,true);
 
     }
 
