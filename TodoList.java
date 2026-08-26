3ba4e05f475391669d86e12ba4843ec7bfe2fad2
Zaine Mac <zaineirshad.07@gmail.com>
2026-08-26 09:26:53 +0100

    trying to get the green

--- TodoList.java
+++ TodoList.java
@@ -5,10 +5,16 @@ Ideas
 3. Categories or tags
 4. Recurring tasks
 5. Seeing cleared tasks
-6. GUI
+6. Storing tasks externally
+7. GUI
 */
+import java.util.Scanner;
+import java.util.ArrayList;
+
 public class TodoList {
     public static void main(String[] args) {
-        
+        ArrayList<task> taskManager = new ArrayList<task>();
+        Scanner sc = new Scanner(System.in);
+        mainMenu(taskManager,sc);
     }
 }
