5770bf9da666cd704ad594d48fdd7463b2c6c61b
Zaine Mac <zaineirshad.07@gmail.com>
2026-08-27 07:35:41 +0100

    Main class for the TodoList V0.1

--- TodoList.java
+++ TodoList.java
@@ -31,9 +31,21 @@ public class TodoList {
             if (choice == 1) {
                 addTask(taskManager, sc);
             } else if (choice == 2) {
-                viewTasks(taskManager, sc);
+                if (taskManager.size() == 0) {
+                    IO.clear();
+                    IO.print("You can't view you tasks are there aren't any tasks to view");
+                    IO.enterPause(sc);
+                } else {
+                    viewTasks(taskManager, sc);
+                }
             } else if (choice == 3) {
-                markTaskComplete(taskManager, sc);
+                if (taskManager.size() == 0) {
+                    IO.clear();
+                    IO.print("Tasks cannot be marked as complete as there are no tasks");
+                    IO.enterPause(sc);
+                } else {
+                    markTaskComplete(taskManager, sc);
+                }
             } else if (choice == 4) {
                 IO.clear();
                 IO.print("Exiting...");
@@ -57,8 +69,10 @@ public class TodoList {
         if (taskManager.size() == 0) {
             IO.print("2. Locked");
             IO.print("3. Stop looking at the locked options AND JUST DO NUMBER 1 OR 4");
-        } else if(){}
-        else {
+        } else if (taskManager.size() == 1) {
+            IO.print("2. View the only task you have");
+            IO.print("3. Mark the only task you have as complete");
+        } else {
             IO.print("2. View tasks");
             IO.print("3. Mark a task as complete");
         }
@@ -66,15 +80,25 @@ public class TodoList {
     }
 
     public static void addTask(ArrayList<task> taskManager, Scanner sc) {
-        String name = IO.StringPut(sc, "Gimmie the name");
-        String desc = IO.StringPut(sc, "Now gimmie discrip");
-        taskManager.add(createTask(name, desc));
-        IO.print("K it's done");
+        IO.clear();
+        String name = IO.StringPut(sc, "Enter the name of your task");
+        String desc = IO.StringPut(sc, "Enter the description or leave blank");
+        if (desc.isEmpty()) {
+            taskManager.add(createTask(name));
+        } else {
+            taskManager.add(createTask(name, desc));
+
+        }
+        IO.print("Task added");
         IO.enterPause(sc);
     }
 
     public static task createTask(String name, String description) {
         return new task(name, description);
+    }
+
+    public static task createTask(String name) {
+        return new task(name);
     }
 
     public static void viewTasks(ArrayList<task> taskManager, Scanner sc) {
@@ -83,7 +107,15 @@ public class TodoList {
             count++;
             IO.print(("Task number: " + count));
             IO.print("Name: " + getName(t));
-            IO.print("Description: " + getDescription(t));
+            try {
+                if (getDescription(t).equals(null)) {
+                    IO.print("There is no description");
+                } else {
+                    IO.print("Description: " + getDescription(t));
+                }
+            } catch (NullPointerException e) {
+                IO.print("There is no description");
+            }
             if (getComplete(t)) {
                 IO.print("Complete: Yes\n");
             } else {
@@ -95,8 +127,14 @@ public class TodoList {
 
     public static void markTaskComplete(ArrayList<task> taskManager, Scanner sc) {
         IO.print("Which task would you like to mark as complete?");
-        for (int i = 0; i < taskManager.size(); i++) {
-            IO.print((i + 1) + ". " + getName(taskManager.get(i)));
+        int count = 0;
+        for (task t : taskManager) {
+            if (getComplete(t) == false) {
+                count++;
+                IO.print((count) + ". " + getName(t));
+            } else {
+                continue;
+            }
         }
         int choice = IO.INTput(sc, "Please enter the number of the task: ");
         if (choice < 1 || choice > taskManager.size()) {
